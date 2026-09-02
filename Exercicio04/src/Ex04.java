8932335b1ecc151d78dddcab0d234577e6671973
Daniveiga22 <danieleradunzdadaveiga@gmail.com>
2026-09-01 21:15:13 -0300

    atualiza

---
+++ Exercicio04/src/Ex04.java
@@ -0,0 +1,23 @@
+public class Ex04 {
+    public static void main(String[] args) {
+
+        Circulo circulo1 = new Circulo();
+        circulo1.raio = 2;
+
+        System.out.println("Raio de "+circulo1.raio);
+
+        Circulo circulo2 = new Circulo();
+        circulo2.raio = 7;
+        System.out.println("Raio de "+circulo2.raio);
+
+        Circulo circulo3 = new Circulo();
+        circulo3.raio = 3;
+        System.out.println("Raio de "+circulo3.raio);
+
+        Circulo circulo4 = new Circulo();
+        circulo4.raio = 4;
+        System.out.println("Raio de "+circulo4.raio);
+
+
+    }
+}
