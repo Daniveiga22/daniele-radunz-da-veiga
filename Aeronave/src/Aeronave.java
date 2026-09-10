c3312ebc03e5352d9586f310e3d7f4168a3246bb
Daniveiga22 <danieleradunzdadaveiga@gmail.com>
2026-09-09 21:18:59 -0300

    atualiza

--- Aeronave/src/Aeronave.java
+++ Aeronave/src/Aeronave.java
@@ -6,4 +6,13 @@ public class Aeronave {
     double capacidadeCombustivel;
     double queimaCombustivelPorMinuto;
 
+    public Aeronave(String modelo, int numeroPassageiros, double velocidadeMaxima, double capacidadeCombustivel, double queimaCombustivelPorMinuto) {
+        this.modelo = modelo;
+        this.numeroPassageiros = numeroPassageiros;
+        this.velocidadeMaxima = velocidadeMaxima;
+        this.capacidadeCombustivel = capacidadeCombustivel;
+        this.queimaCombustivelPorMinuto = queimaCombustivelPorMinuto;
+
+        
+    }
 }
--- Aeronave/src/Ex07.java
+++ Aeronave/src/Ex07.java
@@ -4,29 +4,9 @@ public class Ex07 {
 
         Aeronave Boeing = new Aeronave();
 
-        Boeing.modelo = "Boeing 737";
-        Boeing.numeroPassageiros = ;
-        Boeing.velocidadeMaxima =;
-        Boeing.capacidadeCombustivel = ;
-        Boeing.queimaCombustivelPorMinuto = ;
-
-
         Aeronave Airbus = new Aeronave();
 
-        Airbus.modelo = "";
-        Airbus.numeroPassageiros = ;
-        Airbus.velocidadeMaxima =;
-        Airbus.capacidadeCombustivel = ;
-        Airbus.queimaCombustivelPorMinuto = ;
-
-
         Aeronave Jatinho = new Aeronave();
-
-        Jatinho.modelo = "";
-        Jatinho.numeroPassageiros = ;
-        Jatinho.velocidadeMaxima =;
-        Jatinho.capacidadeCombustivel = ;
-        Jatinho.queimaCombustivelPorMinuto = ;
 
         Aeronave
 
